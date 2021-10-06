80098294b3e70cc79d02e1e9032052e072f72e2c
Christine <christine1@student.umn.ac.id>
2021-10-06 21:44:18 +0700

    fields required register

--- newsProject/controller/register.php
+++ newsProject/controller/register.php
@@ -2,47 +2,51 @@
   include "include/db_connection.php";
 
     if( isset($_POST["submit"]) ) {
-        if(strcmp($_POST['password'], $_POST['konfirmasiPassword']) != 0) {
-            echo "<script>
-                alert('Password salah');
-                document.location.href='?view=register';
-            </script>";
-            die;
+            if(empty($_POST['namaDepan']) || empty($_POST['namaBelakang']) || empty($_POST['username']) || empty($_POST['tgl_lahir']) || empty($_POST['jenisKelamin']) || empty($_POST['password']) || empty($_POST['konfirmasiPassword'])){
+                echo '<script>alert("All fields are required")</script>';
+            }else{
+            if(strcmp($_POST['password'], $_POST['konfirmasiPassword']) != 0) {
+                echo "<script>
+                    alert('Password salah');
+                    document.location.href='?view=register';
+                </script>";
+                die;
+            }
+
+            $namaDepan = $_POST["namaDepan"];
+            $namaBelakang = $_POST["namaBelakang"];
+            $userName = $_POST["userName"];
+            $jenisKelamin = $_POST["jenisKelamin"];
+            $tgl_lahir = $_POST["tgl_lahir"];
+            $id = uniqid('');
+            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            //$gambar = upload();
+
+            // if($gambar === false) {
+            //     echo "<script>
+            //         alert(\"Mahasiswa Gagal Ditambahkan!\");
+            //     </script>";
+            //     echo "<script>
+            //         document.location.href='?view=register';
+            //     </script>";
+            //     die;
+            // } 
+
+            $query = "INSERT INTO user VALUE ('$namaDepan', '$namaBelakang', '$userName','$tgl_lahir','$jenisKelamin', '$id', '$password')";
+
+            mysqli_query($db, $query);
+            
+
+            if(mysqli_affected_rows($db) > 0){
+                echo "<script>alert('User berhasil ditambahkan')</script>";
+                header("Location: ?view=login");
+            }else {
+                echo $db->error;
+                echo "<script>alert('gagal registrasi')</script>";
+            }
         }
-        //sffss
-
-        $namaDepan = $_POST["namaDepan"];
-        $namaBelakang = $_POST["namaBelakang"];
-        $userName = $_POST["userName"];
-        $jenisKelamin = $_POST["jenisKelamin"];
-        $tgl_lahir = $_POST["tgl_lahir"];
-        $id = uniqid('');
-        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-        //$gambar = upload();
-
-        // if($gambar === false) {
-        //     echo "<script>
-        //         alert(\"Mahasiswa Gagal Ditambahkan!\");
-        //     </script>";
-        //     echo "<script>
-        //         document.location.href='?view=register';
-        //     </script>";
-        //     die;
-        // } 
-
-        $query = "INSERT INTO user VALUE ('$namaDepan', '$namaBelakang', '$userName','$tgl_lahir','$jenisKelamin', '$id', '$password')";
-
-        mysqli_query($db, $query);
-
- if(mysqli_affected_rows($db) > 0){
-    echo "<script>alert('User berhasil ditambahkan')</script>";
-    header("Location: ?view=login");
- }else {
-     echo $db->error;
-     echo "<script>alert('gagal registrasi')</script>";
- }
-}
-INCLUDE  "view/register.php";
-
-
+    }
+    
+        INCLUDE  "view/register.php";
+    
 ?>
